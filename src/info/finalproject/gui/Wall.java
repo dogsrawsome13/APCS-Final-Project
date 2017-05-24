cde928935236fd5df5c274fc8aa2b0c1cabb67c6
leoryu <leo.m.ryu@gmail.com>
2017-05-24 09:53:39 -0700

    rock and wall are somewhat done lol

---
+++ src/info/finalproject/gui/Wall.java
@@ -0,0 +1,10 @@
+package info.finalproject.gui;
+
+public class Wall extends Rock {
+	
+	public Wall(double x, double y, int width, int height, String imageName, Board board) {
+		super(x, y, width, height, imageName, board);
+		
+	}
+
+}
