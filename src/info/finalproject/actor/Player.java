f29470b1a76b55a89fb016b87a67717be7e5ec1b
leoryu <leo.m.ryu@gmail.com>
2017-05-23 18:21:53 -0700

    the bullet collision now works! However, the hit() method is consistent for some reason

--- src/info/finalproject/actor/Player.java
+++ src/info/finalproject/actor/Player.java
@@ -5,7 +5,9 @@ import java.awt.event.KeyEvent;
 import java.util.ArrayList;
 
 import info.finalproject.gui.Board;
+import info.finalproject.weapon.MachineGun;
 import info.finalproject.weapon.Pistol;
+import info.finalproject.weapon.RPG;
 import info.finalproject.weapon.Weapon;
 
 public class Player extends Actor {
@@ -13,76 +15,93 @@ public class Player extends Actor {
 	private int mySpeed;
 	private int tmpLoad;
 	private Weapon myWeapon;
-	private ArrayList<Weapon> bullets;
-	private boolean moveForward, canForward, canBackward, moveBackward, left,
-    right, fire, special;
-	
-	public Player(int x, int y, double degrees, int width, int height, String imageName, 
-			int speed, Weapon weapon, Board board) {
+	private ArrayList<Pistol> pistols;
+	private ArrayList<MachineGun> machineGuns;
+	private ArrayList<RPG> rpgs;
+
+	private boolean moveForward, canForward, canBackward, moveBackward, left, right, fire, special;
+
+	public Player(int x, int y, double degrees, int width, int height, String imageName, int speed, Weapon weapon,
+			Board board) {
 		super(x, y, degrees, width, height, imageName, board);
-		myHealth = 100;
+		myHealth = 1000;
 		mySpeed = speed;
-		bullets = new ArrayList<Weapon>();
+		pistols = new ArrayList<Pistol>();
+		machineGuns = new ArrayList<MachineGun>();
+		rpgs = new ArrayList<RPG>();
 		tmpLoad = 0;
 		myWeapon = weapon;
 	}
-	
+
 	public int getHealth() {
 		return myHealth;
 	}
-	
+
 	public void loseHealth(int weaponType) {
 		myHealth = myHealth - weaponType;
 	}
-	
+
 	public int getSpeed() {
 		return mySpeed;
 	}
-	
+
 	public Weapon getWeapon() {
 		return myWeapon;
 	}
-	
+
 	public void setWeapon(Weapon weapon) {
 		myWeapon = weapon;
 	}
-    public ArrayList<Weapon> getBullets() {
-    	return bullets;
-    }
-	
-    public void moveForward(int sx, int sy) {
-        super.setX(super.getX() + Math.cos(super.getDirection()) * sx);
-        super.setY(super.getY() + Math.sin(super.getDirection()) * sy);
-    }
-    public void moveBackward(int sx, int sy) {
-    	super.setX(super.getX() - Math.cos(super.getDirection()) * sx);
-        super.setY(super.getY() - Math.sin(super.getDirection()) * sy);
-    }
 
-    
-    
-    public void fire(int load, int number, int spread) {
-        
-        // if reloading time is done
-        if (tmpLoad == 0) { 
-           for (int i = 0; i < number; i++) {
-              // setting the bullet
-              Board.bullet.setX(super.getX() + super.getWidth());
-              Board.bullet.setY(super.getY() + super.getHeight() / 2);
-              Board.bullet.setDirection(Math.toDegrees(super.getDirection()));
-              Board.bullet.setWidth(10);
-              Board.bullet.setHeight(10);
-              // adding the bullet to the array list
-              bullets.add(new Weapon(Board.bullet.getX(), Board.bullet.getY(), Board.bullet.getDirection(), 
-                      Board.bullet.getWidth(), Board.bullet.getHeight(), "images/missile.png", getBoard()));
-           }
-           //reset the reload time 
-           tmpLoad = load;
-        }
-        else
-           tmpLoad -= 1;  
-    }
-    public String toString() {
-    	return super.toString() + ", " + myWeapon;
-    }
+	public ArrayList getBullets() {
+		if (myWeapon instanceof Pistol)
+			return pistols;
+		if (myWeapon instanceof MachineGun)
+			return machineGuns;
+		else
+			return rpgs;
+	}
+
+	public void moveForward(int sx, int sy) {
+		super.setX(super.getX() + Math.cos(super.getDirection()) * sx);
+		super.setY(super.getY() + Math.sin(super.getDirection()) * sy);
+	}
+
+	public void moveBackward(int sx, int sy) {
+		super.setX(super.getX() - Math.cos(super.getDirection()) * sx);
+		super.setY(super.getY() - Math.sin(super.getDirection()) * sy);
+	}
+
+	public void fire(int load, int number, int spread) {
+
+		// if reloading time is done
+		if (tmpLoad == 0) {
+			for (int i = 0; i < number; i++) {
+				// setting the bullet
+				Board.bullet.setX(super.getX() + super.getWidth());
+				Board.bullet.setY(super.getY() + super.getHeight() / 2);
+				Board.bullet.setDirection(Math.toDegrees(super.getDirection()));
+				Board.bullet.setWidth(10);
+				Board.bullet.setHeight(10);
+				// adding the bullet to the array list
+				if (myWeapon instanceof Pistol)
+					pistols.add(new Pistol(Board.bullet.getX(), Board.bullet.getY(), Board.bullet.getDirection(),
+							Board.bullet.getWidth(), Board.bullet.getHeight(), "images/missile.png", getBoard()));
+				if (myWeapon instanceof MachineGun)
+					machineGuns.add(new MachineGun(Board.bullet.getX(), Board.bullet.getY(),
+							Board.bullet.getDirection(), Board.bullet.getWidth(), Board.bullet.getHeight(),
+							"images/missile.png", getBoard()));
+				else
+					rpgs.add(new RPG(Board.bullet.getX(), Board.bullet.getY(), Board.bullet.getDirection(),
+							Board.bullet.getWidth(), Board.bullet.getHeight(), "images/missile.png", getBoard()));
+			}
+			// reset the reload time
+			tmpLoad = load;
+		} else
+			tmpLoad -= 1;
+	}
+
+	public String toString() {
+		return super.toString() + ", " + myWeapon;
+	}
 }
--- src/info/finalproject/actor/Powerup.java
+++ src/info/finalproject/actor/Powerup.java
@@ -4,27 +4,30 @@ import info.finalproject.gui.*;
 import info.finalproject.weapon.*;
 
 public class Powerup extends Actor {
-	
+
 	private double powerup;
-	
+
 	public Powerup(double x, double y, double direction, int w, int h, String imageName, Board board) {
 		super(x, y, direction, w, h, imageName, board);
 		powerup = (Math.random() * 3);
-		
+
 	}
+
 	public Actor givePowerup() {
-		//if(powerup <= 1)
-			//return new Pistol(this.getX(), this.getY(), this.getDirection(), this.getWidth(), this.getHeight(), "Pistol.png", getBoard());
-		//else
-		if(powerup <= 2)
-			return new MachineGun(this.getX(), this.getY(), this.getDirection(), 
-					this.getWidth(), this.getHeight(), "images/missile.png", getBoard());
-		else if(powerup <= 3)
-			return new RPG(this.getX(), this.getY(), this.getDirection(), 
-					this.getWidth(), this.getHeight(), "images/missile.png", getBoard());
+		// if(powerup <= 1)
+		// return new Pistol(this.getX(), this.getY(), this.getDirection(),
+		// this.getWidth(), this.getHeight(), "Pistol.png", getBoard());
+		// else
+		if (powerup <= 2)
+			return new MachineGun(this.getX(), this.getY(), this.getDirection(), this.getWidth(), this.getHeight(),
+					"images/missile.png", getBoard());
+		else if (powerup <= 3)
+			return new RPG(this.getX(), this.getY(), this.getDirection(), this.getWidth(), this.getHeight(),
+					"images/missile.png", getBoard());
 		else
 			return null;
 	}
+
 	public Powerup removeSelf() {
 		setVisible(false);
 		return this;
